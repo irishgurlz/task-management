156b729d740d0df1a11fa0ffbc7fea09110c4470
irishgurlz <email andagit config --global user.email email>
2026-06-19 21:49:28 +0700

    feat: endpoint login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -48,3 +48,59 @@ export const register = async (req, res) => {
         console.log(error);
     }
 }
+
+export const login = async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "Semua field harus diisi"
+        })
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if (!user) {
+            res.status(404).json({
+                message: "User tidak ditemukan"
+            })
+        }
+
+        const matched = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!matched) {
+            return res.status(400).json({
+                message: "Email dan Password tidak cocok"
+            })
+        }
+
+        const token = jwt.sign(
+            { id: user.id,
+              role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        res.status(200).json({
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            },
+            token
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: error.message
+        })
+    }
+}
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import rateLimit from "express-rate-limit";
-import { register} from '../controllers/auth.controller.js'    
+import { register, login} from '../controllers/auth.controller.js'    
 import authMiddleware from '../middleware/auth.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', register)
+userRouter.post('/login', login)
+
 export default userRouter
